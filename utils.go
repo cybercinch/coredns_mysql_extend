db276e84228863598afec7f8e432ff7f5b460882
Aaron Guise <aaron@guise.net.nz>
2025-08-15 11:23:26 +1200

    feat: add Authority section support to DNS responses
    - Modified MakeMessage() to accept authority parameter
    - Added getAuthorityRecords() to fetch NS records for zones
    - Added getGlueRecords() to fetch A/AAAA records for authority nameservers
    - Fixed zone apex record lookup (empty string → '@')
    - Enhanced debug logging throughout authority record processing
    
    This enables BIND-compatible DNS responses with proper Authority and Additional sections. 🏗️

--- utils.go
+++ utils.go
@@ -12,10 +12,12 @@ func MakeMysqlPlugin() *Mysql {
 	return &Mysql{}
 }
 
-func MakeMessage(r *dns.Msg, answers []dns.RR) *dns.Msg {
+func MakeMessage(r *dns.Msg, answers []dns.RR, authority []dns.RR) *dns.Msg {
 	msg := new(dns.Msg)
 	msg.SetReply(r)
 	msg.Answer = answers
+	msg.Ns = authority // Add authority section
+	msg.Authoritative = true // Set AA flag
 	return msg
 }
 
@@ -110,4 +112,106 @@ func (m *Mysql) makeAnswer(rrString string) (dns.RR, error) {
 		makeAnswerCount.With(prometheus.Labels{"status": "success"}).Inc()
 	}
 	return rr, nil
+}
+
+// getAuthorityRecords fetches NS records for the zone to include in Authority section
+func (m *Mysql) getAuthorityRecords(zoneID int, zone string) []dns.RR {
+	var authority []dns.RR
+	
+	logDebugf("getAuthorityRecords called: zoneID=%d, zone='%s'", zoneID, zone)
+	
+	if zoneID <= 0 {
+		logDebugf("getAuthorityRecords: invalid zoneID %d, returning empty authority", zoneID)
+		return authority
+	}
+	
+	// Fetch NS records for this zone (use zoneSelf "@" for apex records)
+	nsRecords, err := m.getRecords(zoneID, zoneSelf, zone, "NS")
+	if err != nil {
+		logDebugf("Failed to fetch NS records for authority section: %v", err)
+		return authority
+	}
+	
+	logDebugf("Found %d NS records for authority section in zone %s", len(nsRecords), zone)
+	
+	for i, nsRecord := range nsRecords {
+		nsRRString := fmt.Sprintf("%s %d IN NS %s", nsRecord.zoneName, nsRecord.ttl, nsRecord.data)
+		logDebugf("Adding NS authority record %d/%d: %s", i+1, len(nsRecords), nsRRString)
+		
+		nsRR, err := m.makeAnswer(nsRRString)
+		if err != nil {
+			logErrorf("Failed to create NS authority record: %v", err)
+			continue
+		}
+		authority = append(authority, nsRR)
+	}
+	
+	logDebugf("getAuthorityRecords returning %d authority records for zone %s", len(authority), zone)
+	return authority
+}
+
+// getGlueRecords fetches A/AAAA records for nameservers in the authority section
+func (m *Mysql) getGlueRecords(authority []dns.RR) []dns.RR {
+	var glueRecords []dns.RR
+	
+	logDebugf("getGlueRecords called with %d authority records", len(authority))
+	
+	for i, authRR := range authority {
+		if authRR.Header().Rrtype != dns.TypeNS {
+			continue
+		}
+		
+		ns := authRR.(*dns.NS).Ns
+		logDebugf("Looking for glue records for NS %d/%d: %s", i+1, len(authority), ns)
+		
+		// Try to find the zone that contains this nameserver
+		nsZoneID, nsHost, nsZone, err := m.getDomainInfo(ns)
+		if err != nil {
+			logDebugf("No zone found for NS %s (external): %v", ns, err)
+			continue // External nameserver, no glue needed
+		}
+		
+		logDebugf("Found zone for NS %s -> zoneID=%d, host='%s', zone='%s'", ns, nsZoneID, nsHost, nsZone)
+		
+		// Fetch A records for this nameserver
+		aRecords, err := m.getRecords(nsZoneID, nsHost, nsZone, "A")
+		if err == nil {
+			logDebugf("Found %d A glue records for NS %s", len(aRecords), ns)
+			for j, aRecord := range aRecords {
+				glueRRString := fmt.Sprintf("%s %d IN A %s", aRecord.fqdn, aRecord.ttl, aRecord.data)
+				logDebugf("Adding A glue record %d/%d: %s", j+1, len(aRecords), glueRRString)
+				
+				glueRR, err := m.makeAnswer(glueRRString)
+				if err == nil {
+					glueRecords = append(glueRecords, glueRR)
+				} else {
+					logErrorf("Failed to create A glue record: %v", err)
+				}
+			}
+		} else {
+			logDebugf("No A glue records for NS %s: %v", ns, err)
+		}
+		
+		// Fetch AAAA records for this nameserver
+		aaaaRecords, err := m.getRecords(nsZoneID, nsHost, nsZone, "AAAA")
+		if err == nil {
+			logDebugf("Found %d AAAA glue records for NS %s", len(aaaaRecords), ns)
+			for j, aaaaRecord := range aaaaRecords {
+				glueRRString := fmt.Sprintf("%s %d IN AAAA %s", aaaaRecord.fqdn, aaaaRecord.ttl, aaaaRecord.data)
+				logDebugf("Adding AAAA glue record %d/%d: %s", j+1, len(aaaaRecords), glueRRString)
+				
+				glueRR, err := m.makeAnswer(glueRRString)
+				if err == nil {
+					glueRecords = append(glueRecords, glueRR)
+				} else {
+					logErrorf("Failed to create AAAA glue record: %v", err)
+				}
+			}
+		} else {
+			logDebugf("No AAAA glue records for NS %s: %v", ns, err)
+		}
+	}
+	
+	logDebugf("getGlueRecords returning %d glue records", len(glueRecords))
+	return glueRecords
 }
